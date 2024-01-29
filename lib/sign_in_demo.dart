ad574c59ee13f0296abb781a1f108a19f5d1032a
Crosline <cagkancaglayanel@gmail.com>
2024-01-30 01:07:37 +0300

    Add: Google Auth options

---
+++ lib/sign_in_demo.dart
@@ -0,0 +1,111 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart' show GoogleSignInAccount;
+import 'package:panteon_cocktail_menu/google_options.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      title: 'Google Sign In',
+      home: SignInDemo(),
+    ),
+  );
+}
+
+/// The SignInDemo app.
+class SignInDemo extends StatefulWidget {
+  ///
+  const SignInDemo({super.key});
+
+  @override
+  State createState() => _SignInDemoState();
+}
+
+class _SignInDemoState extends State<SignInDemo> {
+  GoogleSignInAccount? _currentUser;
+
+  @override
+  void initState() {
+    super.initState();
+
+    DefaultGoogleOptions.currentPlatform.onCurrentUserChanged
+        .listen((GoogleSignInAccount? account) async {
+
+      setState(() {
+        _currentUser = account;
+      });
+    });
+
+    // In the web, _googleSignIn.signInSilently() triggers the One Tap UX.
+    //
+    // It is recommended by Google Identity Services to render both the One Tap UX
+    // and the Google Sign In button together to "reduce friction and improve
+    // sign-in rates" ([docs](https://developers.google.com/identity/gsi/web/guides/display-button#html)).
+    DefaultGoogleOptions.currentPlatform.signInSilently();
+  }
+
+  // This is the on-click handler for the Sign In button that is rendered by Flutter.
+  //
+  // On the web, the on-click handler of the Sign In button is owned by the JS
+  // SDK, so this method can be considered mobile only.
+  // #docregion SignIn
+  Future<void> _handleSignIn() async {
+    try {
+      await DefaultGoogleOptions.currentPlatform.signIn();
+    } catch (error) {
+      print(error);
+    }
+  }
+  // #enddocregion SignIn
+
+  Future<void> _handleSignOut() => DefaultGoogleOptions.currentPlatform.disconnect();
+
+  Widget _buildBody() {
+    final GoogleSignInAccount? user = _currentUser;
+    if (user != null) {
+      // The user is Authenticated
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          ListTile(
+              leading: const CircleAvatar(
+
+              ),
+              title: Text(user.displayName ?? user.email.split("@")[0])
+          ),
+          const Text('Signed in successfully.'),
+          ElevatedButton(
+            onPressed: _handleSignOut,
+            child: const Text('SIGN OUT'),
+          ),
+        ],
+      );
+    } else {
+      // The user is NOT Authenticated
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          const Text('You are not currently signed in.'),
+          // This method is used to separate mobile from web code with conditional exports.
+          // See: src/sign_in_button.dart
+          ElevatedButton(
+            onPressed: _handleSignIn,
+            child: const Text('SignIn'),
+          ),
+        ],
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Google Sign In'),
+        ),
+        body: ConstrainedBox(
+          constraints: const BoxConstraints.expand(),
+          child: _buildBody(),
+        ));
+  }
+}
