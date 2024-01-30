787e566f56026acb84fe6cef308d2c81087c1c9e
Crosline <cagkancaglayanel@gmail.com>
2024-01-30 20:34:12 +0300

    Add: some new stuff

--- lib/controllers/firebase_controller.dart
+++ lib/controllers/firebase_controller.dart
@@ -1,4 +1,7 @@
 
+import 'dart:async';
+import 'dart:html';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
 
@@ -6,12 +9,66 @@ import '../options/firebase_options.dart';
 
 class FirebaseController {
 
+  final StreamController _streamController =
+  StreamController.broadcast();
+  Stream get onInitialized {
+    return _streamController.stream;
+  }
+
   static const USE_DATABASE_EMULATOR = false;
   static const emulatorPort = 9000;
   final emulatorHost ='localhost';
 
+  late final FirebaseDatabase _database;
+  late final DatabaseReference _admins;
+
+  late bool _initialized;
+
   FirebaseController() {
+    _initialized = false;
     _initialize();
+  }
+
+  Future<List<String>> _getAdminList() async {
+    final adminSnapshot = await _admins.get();
+
+    if (adminSnapshot.value == null) {
+      return <String>[];
+    }
+
+    var adminListDynamic = adminSnapshot.value as List<dynamic>;
+
+    var adminList = <String>[];
+    for (String admin in adminListDynamic) {
+      adminList.add(admin);
+    }
+
+    return adminList;
+  }
+
+  Future<bool> isUserAdmin(String? id) async {
+    if (!_initialized) return false;
+    if (id == null) return false;
+
+    try {
+      var adminList = await _getAdminList();
+      return adminList.contains(id);
+    } catch (err) {
+      return false;
+    }
+  }
+
+  Future<void> addAdminUser(String? id) async {
+    if (!_initialized) return;
+    if (id == null) return;
+
+     var adminList = await _getAdminList();
+    if (adminList.contains("id")) return;
+
+    adminList.add(id);
+
+    await _admins
+        .set(adminList);
   }
 
   Future<void> _initialize() async {
@@ -19,8 +76,16 @@ class FirebaseController {
       options: DefaultFirebaseOptions.currentPlatform,
     );
 
+    _database = FirebaseDatabase.instance;
+    _admins = _database.ref("cbo_admins");
+
     if (USE_DATABASE_EMULATOR) {
-      FirebaseDatabase.instance.useDatabaseEmulator(emulatorHost, emulatorPort);
+      _database.useDatabaseEmulator(emulatorHost, emulatorPort);
+    }
+
+    _initialized = true;
+    if (_streamController.hasListener) {
+      _streamController.add(null);
     }
   }
 }
