c27d8eca7b16a0e711fe6c2b111f95c68eb090e1
Crosline <cagkancaglayanel@gmail.com>
2024-02-01 00:48:16 +0300

    Update: changed the main app title [deploy]

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Panteon Cocktail',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.red,
