e01d1c7f767672e18b686520a76323ba28649ecb
Yiğitcan Öksüz <yigitcanoksuz@panteon.games>
2024-02-15 13:37:49 +0300

    add order imp

--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -1,11 +1,50 @@
-
 import 'package:flutter/material.dart';
+import 'package:panteon_cocktail_menu/main.dart';
+import 'package:panteon_cocktail_menu/models/cocktail.dart';
+import 'package:panteon_cocktail_menu/models/order.dart';
 
-class ProductCard extends StatelessWidget {
+class ProductCard extends StatefulWidget {
   const ProductCard({Key? key, required this.title, required this.image})
       : super(key: key);
   final String title;
   final String image;
+
+  @override
+  State<ProductCard> createState() => _ProductCardState();
+}
+
+class _ProductCardState extends State<ProductCard> {
+  int _quantity = 1;
+  String _name = "";
+
+  @override
+  void initState() {
+    super.initState();
+
+    //fetch coctails
+  }
+
+  void _setOrder() {
+    var order = Cocktail(name: _name);
+    firebaseController.addNewOrder(Order(
+        accountName: signInController.currentUser!.displayName!,
+        cocktails: <Cocktail, int>{order: _quantity},
+        orderTime: DateTime.now()));
+  }
+
+  void _increment() {
+    setState(() {
+      _quantity++;
+    });
+  }
+
+  void _decrement() {
+    setState(() {
+      if (_quantity > 1) {
+        _quantity--;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,28 +57,41 @@ class ProductCard extends StatelessWidget {
             Expanded(
                 flex: 30,
                 child: Image.asset(
-                  image,
+                  widget.image,
                   fit: BoxFit.contain,
                   height: 100,
                 )),
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Text(
-                title,
+                widget.title,
                 style: Theme.of(context).textTheme.titleLarge,
               ),
             ),
             const Spacer(),
             const Text("Lime, Tequila, Triple Sec"),
-            Align(
-              alignment: Alignment.bottomRight,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: ElevatedButton(
-                  style: Theme.of(context).elevatedButtonTheme.style,
-                  onPressed: () {},
-                  child: const Text('Order'),
-                ),
+            Padding(
+              padding: EdgeInsets.only(left: 8.0, right: 8.0, bottom: 8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Row(children: <Widget>[
+                    IconButton.outlined(
+                      onPressed: _decrement,
+                      icon: const Icon(Icons.remove),
+                    ),
+                    Text(_quantity.toString()),
+                    IconButton.outlined(
+                      onPressed: _increment,
+                      icon: const Icon(Icons.add),
+                    ),
+                  ]),
+                  ElevatedButton(
+                    style: Theme.of(context).elevatedButtonTheme.style,
+                    onPressed: _setOrder,
+                    child: const Text('Order'),
+                  ),
+                ],
               ),
             )
           ],
